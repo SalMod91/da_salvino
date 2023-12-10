1d7b8a68c60c1a53b7aaf277fbc379f48b7368d7
Salvino Modica <salvymody@gmail.com>
2023-12-10 18:03:55 +0000

    refactor: remade sticky animation

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,9 @@
-// Assigns Id: 'header-not-at-top' to <header></header> when the position is not at top:0
+// Assigns Class: 'sticky-top' to <header> when the position is not at top:0
 window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
-    if (window.scrollY > 0) {
-        header.setAttribute('id', 'header-not-at-top');
+    if (window.scrollY > 50) {
+        header.setAttribute('class', 'sticky-top');
     } else {
-        header.removeAttribute('id');
+        header.removeAttribute('class');
     }
 });
