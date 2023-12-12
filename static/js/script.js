67273f43e4d50089f5f2ca9ab393bfec5073ad09
Salvino Modica <salvymody@gmail.com>
2023-12-12 10:46:28 +0000

    refactor: updated js comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,15 @@
-// Assigns Class: 'sticky-top' to <header> when the position is not at top:0
+// Listen for the scroll event
 window.addEventListener('scroll', function() {
+
+    // Select the header element
     var header = document.querySelector('header');
+
+    // Check if the scroll position is greater than 50 pixels
     if (window.scrollY > 50) {
+        // If true, add 'sticky-top' class to make the header sticky
         header.setAttribute('class', 'sticky-top');
     } else {
+        // If false, remove the class to position the header on top
         header.removeAttribute('class');
     }
 });
