16e0ab617590e267653ae812e220235b0788437b
Salvino Modica <salvymody@gmail.com>
2024-01-02 06:48:59 +0000

    feat: created initial Ingredient model

--- staff_portal/models.py
+++ staff_portal/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class IngredientCategory(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    category = models.ForeignKey(IngredientCategory, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    image = CloudinaryField('image', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
