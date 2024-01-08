148eefd88d7a6b448eeb7380d93aeb4fe271b520
Salvino Modica <salvymody@gmail.com>
2024-01-08 12:33:12 +0000

    feat: add delete cloudinary image

--- staff_portal/models.py
+++ staff_portal/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.conf import settings
 from cloudinary.models import CloudinaryField
-from cloudinary.uploader import upload
+from cloudinary.uploader import upload, destroy
 
 
 class IngredientCategory(models.Model):
@@ -28,7 +28,7 @@ class Ingredient(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Overrides the save method to handle image uploads. If an image file is present,
+        Overrides the default save method to handle image uploads. If an image file is present,
         it saves the ingredient without the image, uploads the image to Cloudinary to
         generate a public ID, and then saves the ingredient again with the updated image URL.
         This approach ensures that the image is stored with a consistent naming convention
@@ -53,3 +53,20 @@ class Ingredient(models.Model):
         else:
             # If it's not a file, just save the model as is
             super(Ingredient, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """
+        Overrides the default delete method to handle the
+        deletion of the associated image from Cloudinary.
+        When an ingredient is deleted, this method checks if there is an associated image.
+        If there is, it constructs the public_id of the image based on the ingredient's ID
+        and the predefined folder path ('ingredients').
+        It then calls the Cloudinary API to delete the image from the cloud storage.
+        After deleting the image, it proceeds with deleting the Ingredient instance from the database.
+        """
+
+        if self.image:
+            public_id = f'ingredients/ingredient_image_{self.id}'
+            destroy(public_id)
+
+        super(Ingredient, self).delete(*args, **kwargs)
