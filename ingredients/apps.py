049e1c492a056d10ebaf29e6c3364621250d60d2
Salvino Modica <salvymody@gmail.com>
2024-02-05 10:24:13 +0000

    refactor: improved python structure ingredients app

--- ingredients/apps.py
+++ ingredients/apps.py
@@ -3,7 +3,8 @@ from django.apps import AppConfig
 
 class IngredientsConfig(AppConfig):
     """
-    Configuration for the 'ingredients' app with BigAutoField as default ID field
+    Configuration for the 'ingredients' app with BigAutoField
+    as default ID field
     """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ingredients'
--- ingredients/views.py
+++ ingredients/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from staff_portal.models import Ingredient, IngredientCategory
 
+
 class IngredientsPage(TemplateView):
     """
-    A class-based view for rendering the ingredients page using Django's class TemplateView.
-    Enables the Template to display dynamic data.
+    A class-based view for rendering the ingredients page using Django's class
+    TemplateView. Enables the Template to display dynamic data.
     """
 
     # Specifies the template to be used when rendering this view
@@ -18,27 +19,28 @@ class IngredientsPage(TemplateView):
         It retrieves the context from its parent class (TemplateView) and
         customizes its content with the ingredient categories and ingredients.
         """
-        # Calls the get_context_data method from its parent class, "TemplateView"
+        # Calls the get_context_data method from parent class "TemplateView"
         context = super().get_context_data(**kwargs)
-        
+
         # Creating an empty dictionary to hold categories and their ingredients
         categorized_ingredients = {}
 
         # Retrieve all categories
         categories = IngredientCategory.objects.all()
 
-        # Iterate over each category and retrieve its ingredients
-        # For each category, filter the ingredients from the database that belong to that category
-        # and store them in the categorized_ingredients dictionary with the category name as the key
+        # Iterate over each category and retrieve its ingredients.
+        # For each category, filter the ingredients from the database
+        # that belong to that category and store them in the
+        # categorized_ingredients dictionary with the category name as the key
         for category in categories:
             ingredients = Ingredient.objects.filter(
-                category=category
-            # Orders ingredients alphabetically by name
-            ).order_by('name')
+                    category=category
+                ).order_by('name')  # Orders ingredients alphabetically by name
             categorized_ingredients[category.name] = ingredients
 
         # Add categorized ingredients dictionary to the context dictionary
         context['categorized_ingredients'] = categorized_ingredients
 
-        # Returns the context dictionary wich now includes the 'categorized_ingredients'
+        # Returns the context dictionary wich now includes
+        # the 'categorized_ingredients'
         return context
